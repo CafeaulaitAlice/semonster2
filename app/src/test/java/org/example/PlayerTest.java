ab48688dee56c22288956c2c0c585a7f075f15fa
FutaSaito <zeno1118f@gmail.com>
2024-07-16 14:44:51 +0900

    add Player class

---
+++ app/src/test/java/org/example/PlayerTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class PlayerTest {
+  @Test
+  public void appHasAGreeting() {
+    Player classUnderTest = new Player();
+  }
+}
